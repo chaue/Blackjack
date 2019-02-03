233cd07c78314a0408d95a580f2f98eaa322a345
Edwin Chau <edwinchau117@gmail.com>
2019-02-02 17:36:52 -0800

    exp class

--- blackjack/card.h
+++ blackjack/card.h
@@ -1,3 +1,4 @@
+#include <map>
 // card class for blackjack
 
 class Card {
@@ -9,4 +10,14 @@ public:
 private:	
 	int value;				// range from 1 to 13
 	int dupe = 4;			// 4 cards per suit
+};
+
+// deck class
+class Deck {
+public:
+	Deck();
+	int get_num_cards() const;
+	void draw();
+private:
+	std::map<int, int> cardlist;
 };
--- blackjack/main.cpp
+++ blackjack/main.cpp
@@ -1,9 +1,22 @@
 #include "card.h"
 #include <iostream>
+#include <map>
 using namespace std;
 
 int main() {
+	// card class works
 	Card c = Card(5);
 	int cval = c.get_value();
 	cout << cval;
+
+	// testing map stuff
+	map<int, int> cards;
+	cards.insert(pair<int, int>(3, 4));
+	cards.insert(pair<int, int>(5, 6));
+	
+	map<int,int>::iterator it = cards.begin();
+	while (it != cards.end()) {
+		cout << it->first << " " << it->second;
+		it++;
+	}
 }
